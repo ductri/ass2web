5f0c7d479fa4846dee8bf152dd7905319c2b618a
ductri <ductriuv@gmail.com>
2016-04-10 11:11:57 +0700

    debug

--- index.php
+++ index.php
@@ -8,25 +8,25 @@ use Phroute\Phroute\Dispatcher;
 echo 'abc';
 $collector = new RouteCollector();
 console_log('abc');
-// console_log($collector);
-// $collector->get('/', function(){
-// 	readfile('html/tuanBD/theme.html');
-// });
+console_log($collector);
+$collector->get('/', function(){
+	readfile('html/tuanBD/theme.html');
+});
 
-// $collector->get('/catalog/{catalog}', function($catalog){
-// 	//Asume
-// 	$catalog = "top-download";
-// 	readfile('html/quang/catalog.html');
-// });
+$collector->get('/catalog/{catalog}', function($catalog){
+	//Asume
+	$catalog = "top-download";
+	readfile('html/quang/catalog.html');
+});
 
-// $collector->get('/catalog/{catalog}/{slideid}', function($catalog, $slideid){
-// 	//Asume
-// 	$catalog = "top-download";
-// 	$slideid = 1;
-// 	readfile('html/tri/index.html');
-// });
-// $dispatcher =  new Dispatcher($collector->getData());
+$collector->get('/catalog/{catalog}/{slideid}', function($catalog, $slideid){
+	//Asume
+	$catalog = "top-download";
+	$slideid = 1;
+	readfile('html/tri/index.html');
+});
+$dispatcher =  new Dispatcher($collector->getData());
 
-// echo $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));   // Home Page
+echo $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));   // Home Page
 
 ?>
