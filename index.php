b67aab88ebc3a6b620494baa3f68e58435d38301
ductri <ductriuv@gmail.com>
2016-04-10 09:28:47 +0700

    ds

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php
-include 'utils.php';
-include "config/config.php";
-include __DIR__ . './vendor/autoload.php';
+// include 'utils.php';
+// include "config/config.php";
+// include __DIR__ . './vendor/autoload.php';
 
-use Phroute\Phroute\RouteCollector;
-use Phroute\Phroute\Dispatcher;
+// use Phroute\Phroute\RouteCollector;
+// use Phroute\Phroute\Dispatcher;
 
-$collector = new RouteCollector();
+// $collector = new RouteCollector();
 
-$collector->get('/', function(){
-	readfile('html/tuanBD/theme.html');
-});
+// $collector->get('/', function(){
+// 	readfile('html/tuanBD/theme.html');
+// });
 
-$collector->get('/catalog/{catalog}', function($catalog){
-	//Asume
-	$catalog = "top-download";
-	readfile('html/quang/catalog.html');
-});
+// $collector->get('/catalog/{catalog}', function($catalog){
+// 	//Asume
+// 	$catalog = "top-download";
+// 	readfile('html/quang/catalog.html');
+// });
 
-$collector->get('/catalog/{catalog}/{slideid}', function($catalog, $slideid){
-	//Asume
-	$catalog = "top-download";
-	$slideid = 1;
-	readfile('html/tri/index.html');
-});
-$dispatcher =  new Dispatcher($collector->getData());
+// $collector->get('/catalog/{catalog}/{slideid}', function($catalog, $slideid){
+// 	//Asume
+// 	$catalog = "top-download";
+// 	$slideid = 1;
+// 	readfile('html/tri/index.html');
+// });
+// $dispatcher =  new Dispatcher($collector->getData());
 
-echo $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));   // Home Page
+// echo $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));   // Home Page
 
 ?>
