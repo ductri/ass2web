dbf9e9b2e9644707248cd14f2800f12aa813db4c
Pham Tran Minh Quang <binquangbk@gmail.com>
2016-04-18 22:28:25 +0700

    Edit Header

---
+++ html/quang/catalog.php
@@ -0,0 +1,386 @@
+<?php
+session_start();
+include(dirname(__FILE__)."\..\header\index.php");
+?>
+
+
+<!-- Body -->
+<img class="img-responsive center-block" src="images/banner4.jpg" alt="banner"/>
+<div class="container">
+  <div class="row text-center center-items">
+    <div id="center" class="col-md-12">
+      <h2>Social Topic</h2>
+      <hr class="catalog"/>
+      <div class="row">
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="public/tri/resources/slide1.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">IoT with<br>NodeJS</p>
+                <p class="author-temple">Kun</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide2.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">React<br>JS</p>
+                <p class="author-temple">Valencia</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide3.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">Champion<br>League</p>
+                <p class="author-temple">Quang Pham</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="clear"></div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide4.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">Facebook<br>Network</p>
+                <p class="author-temple">Steve Jobs</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide5.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">Arenal<br>FC</p>
+                <p class="author-temple">Gunner</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide6.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">Anime<br>Shonen</p>
+                <p class="author-temple">Gintama</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="clear"></div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide7.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">Small things<br>in you</p>
+                <p class="author-temple">NoName</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide8.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">Football<br>&nbsp;</p>
+                <p class="author-temple">Kaka</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-center">
+          <div class="padding-item top-10">
+            <div class="hover-group">
+              <div class="image-wrapper">
+                <a class="link-item" href="../tri/index.html">
+                  <img class="img-responsive center-block item-picture" src="../tri/resources/slide9.png" alt="item" title="Sky temple"/>
+                </a>
+                <div class="hover-toggle btn-group-justified">
+                <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Like" >
+                      <img src="/pub/img/Thumb-Up-48.png" width="24" alt="like">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Download" >
+                      <img src="/pub/img/Download-48.png" width="24" alt="download">
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" title="Share">
+                      <img src="/pub/img/Share-48.png" width="24" alt="share">
+                    </button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              <p class="info text-left no-padding">
+                <span class="glyphicon glyphicon-circle-arrow-down"></span> 1.4k&emsp;
+                <span class="glyphicon glyphicon-star span-item"></span> 2.3k
+              </p>
+              <hr>
+              <div class="col-md-12 text-left no-padding">
+                <p class="title-temple">How to deal<br>with stress</p>
+                <p class="author-temple">Tore</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <hr/>
+      <span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>
+    </div>
+  </div>
+</div>
+</body>
+</html>
