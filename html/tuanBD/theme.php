359af72a3957667be7c9682d5cdb2ab05bffe39d
keohoangphuong <keohoangphuong3003@gmail.com>
2016-05-03 23:25:56 +0700

    Edit Page of Tuan be de

--- html/tuanBD/theme.php
+++ html/tuanBD/theme.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="/pub/js/quang/index.js"></script>
+  <!--<script src="/pub/js/quang/index.js"></script> -->
   <link rel="stylesheet" type="text/css" href="/pub/css/tuan/style.css">
   <style type="text/css"></style>
 </head>
