b5825d6865cce9409e81c9dd2f1873455a253d79
ductri <ductriuv@gmail.com>
2016-05-07 15:06:54 +0700

    temp

--- config/config.php
+++ config/config.php
@@ -5,14 +5,14 @@ DATABASE
 *****************************************************************/
 define("DB_NAME", "ass2");
 // For localhost
-// define("DB_USERNAME", "root");
-// define("DB_PASSWORD", "ductri");
-// define("DB_HOSTNAME", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "ductri");
+define("DB_HOSTNAME", "localhost");
 
 // For openshift
-define("DB_USERNAME", "adminYHxmQA9");
-define("DB_PASSWORD", "EWVI3RILgLzB");
-define("DB_HOSTNAME", "https://ass2-webhcmut.rhcloud.com");
+// define("DB_USERNAME", "adminYHxmQA9");
+// define("DB_PASSWORD", "EWVI3RILgLzB");
+// define("DB_HOSTNAME", "https://ass2-webhcmut.rhcloud.com");
 
 
 /*****************************************************************
